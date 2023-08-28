fa03309acc44e28ffbce2cd9bed930a5af13e433
María Ripoll Vives <mariaripoll95@icloud.com>
2023-08-28 18:08:37 +0000

    Generated the file Names

---
+++ src/js/component/names.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//create your first component
+const Names = () => {
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">Hello World</h1>
+		</div>
+	);
+};
+
+export default Names;
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Names from "./component/names.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Names />, document.querySelector("#app"));
